7dd39ce421b8a7f8fb66d09ea32d247f7c9cb15a
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-03-26 15:19:36 +0100

    ajout de props pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
-      <PokemonCard></PokemonCard>
-     
+      <PokemonCard pokemon={pokemonList[0]}></PokemonCard>
     </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default App;
